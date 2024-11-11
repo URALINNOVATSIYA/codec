4cd6d809b89aa35ea69bd82d5cf1c6882ea1fc0b
AlephTav <4lephtav@gmail.com>
2024-11-11 18:46:56 +0300

    Refactoring all logic

--- graph.go
+++ graph.go
@@ -23,14 +23,6 @@ func (g *graph) nodes() map[int][]int {
 	return g.childs
 }
 
-func (g *graph) children(parentId int) []int {
-	return g.childs[parentId]
-}
-
-func (g *graph) parents(childId int) []int {
-	return g.prnts[childId]
-}
-
 func (g *graph) isVisited(nodeId int) bool {
 	_, exists := g.vmap[nodeId]
 	return exists
@@ -41,5 +33,115 @@ func (g *graph) visit(nodeId int) {
 }
 
 func (g *graph) renumber(currentNodeId, breakNodeId int) {
+	maxNodeId := g.findMaxNodeId(breakNodeId, breakNodeId, breakNodeId, make(map[int]struct{}))
+	
+	inc := currentNodeId - maxNodeId
+	dec := maxNodeId - breakNodeId + 1
+	g.renumberBorderNodes(maxNodeId+1, breakNodeId, maxNodeId, inc, dec, g.prnts, g.childs)
+	prnts := g.renumberParents(breakNodeId, currentNodeId, maxNodeId, inc, dec)
+	childs := g.renumberChilds(breakNodeId, currentNodeId, maxNodeId, inc, dec, prnts)
+	
+	currentNodeId -= dec
+	breakNodeId += inc
+	prnts[currentNodeId] = append(prnts[currentNodeId], prnts[breakNodeId]...)
+	prnts[breakNodeId] = []int{currentNodeId}
+	childs[currentNodeId] = []int{breakNodeId}
 
+	g.merge(g.childs, childs)
+	g.merge(g.prnts, prnts)
+}
+
+func (g *graph) findMaxNodeId(parentNodeId, minNodeId, maxNodeId int, vmap map[int]struct{}) int {
+	for _, nodeId := range g.childs[parentNodeId] {
+		if nodeId <= minNodeId {
+			continue
+		}
+		if _, visited := vmap[nodeId]; visited {
+			continue
+		}
+		vmap[nodeId] = struct{}{}
+		if nodeId > maxNodeId {
+			maxNodeId = nodeId
+		}
+		maxNodeId = g.findMaxNodeId(nodeId, minNodeId, maxNodeId, vmap)
+	}
+	return maxNodeId
+}
+
+func (g *graph) renumberBorderNodes(borderNodeId, startNodeId, turnNodeId, inc, dec int, childs, prnts map[int][]int) {
+	for _, childId := range childs[borderNodeId] {
+		if childId >= startNodeId {
+			continue
+		}
+		elems := prnts[childId]
+		for i, nodeId := range elems {
+			if nodeId >= startNodeId {
+				elems[i] = g.renumberNodeId(nodeId, turnNodeId, inc, dec)
+			}
+		}
+	}
+}
+
+func (g *graph) renumberParents(startNodeId, endNodeId, turnNodeId, inc, dec int) map[int][]int {
+	prnts := make(map[int][]int)
+	for nodeId := startNodeId; nodeId <= endNodeId; nodeId++ {
+		elems := g.prnts[nodeId]
+		if len(elems) == 0 {
+			continue
+		}
+		for i, id := range elems {
+			if id < startNodeId {
+				elems[i] = id
+			} else {
+				elems[i] = g.renumberNodeId(id, turnNodeId, inc, dec)
+			}
+		}
+		prnts[g.renumberNodeId(nodeId, turnNodeId, inc, dec)] = elems
+		delete(g.prnts, nodeId)
+	}
+	return prnts
+}
+
+func (g *graph) renumberChilds(startNodeId, endNodeId, turnNodeId, inc, dec int, prnts map[int][]int) map[int][]int {
+	childs := make(map[int][]int)
+	for nodeId := startNodeId; nodeId <= endNodeId; nodeId++ {
+		elems := g.childs[nodeId]
+		if len(elems) == 0 {
+			continue
+		}
+		for i, id := range elems {
+			if id >= startNodeId {
+				elems[i] = g.renumberNodeId(id, turnNodeId, inc, dec)
+				continue
+			}
+			elems[i] = id
+			parents := g.prnts[id]
+			if parents == nil {
+				continue
+			}
+			for j, parentId := range parents {
+				if parentId >= startNodeId {
+					parents[j] = g.renumberNodeId(parentId, turnNodeId, inc, dec)
+				}
+			}
+			prnts[id] = parents
+			delete(g.prnts, id)
+		}
+		childs[g.renumberNodeId(nodeId, turnNodeId, inc, dec)] = elems
+		delete(g.childs, nodeId)
+	}
+	return childs
+}
+
+func (g *graph) renumberNodeId(nodeId, turnNodeId, inc, dec int) int {
+	if nodeId <= turnNodeId {
+		return nodeId + inc
+	}
+	return nodeId - dec
+}
+
+func (g *graph) merge(nodes1, nodes2 map[int][]int) {
+	for nodeId, nodes := range nodes2 {
+		nodes1[nodeId] = nodes
+	}
 }
--- graph_test.go
+++ graph_test.go
@@ -5,10 +5,11 @@ import (
 	"testing"
 )
 
-func TestGraph_add(t *testing.T) {
-	type node struct {
-		childId, parentId int
-	}
+type node struct {
+	childId, parentId int
+}
+
+func TestGraph_Add(t *testing.T) {
 	items := []struct{
 		nodes []node
 		childs map[int][]int
@@ -38,6 +39,49 @@ func TestGraph_add(t *testing.T) {
 				7: {6},
 			},
 		},
+		// #2
+		{
+			[]node{
+				{1, 0},
+				{2, 1}, {12, 1},
+				{3, 2}, {4, 2}, {5, 2},
+				{4, 5}, {6, 5}, {7, 5},
+				{7, 6}, {8, 6}, {9, 6}, {11, 6},
+				{10, 8},
+				{10, 9},
+				{4, 11}, {5, 11},
+				{13, 12}, {14, 12},
+				{7, 13},
+			},
+			map[int][]int{
+				0: {1},
+				1: {2, 12},
+				2: {3, 4, 5},
+				5: {4, 6, 7},
+				6: {7, 8, 9, 11},
+				8: {10},
+				9: {10},
+				11: {4, 5},
+				12: {13, 14},
+				13: {7},
+			},
+			map[int][]int{
+				1: {0},
+				2: {1},
+				3: {2},
+				4: {2, 5, 11},
+				5: {2, 11},
+				6: {5},
+				7: {5, 6, 13},
+				8: {6},
+				9: {6},
+				10: {8, 9},
+				11: {6},
+				12: {1},
+				13: {12},
+				14: {12},
+			},
+		},
 	}
 	for i, item := range items {
 		graph := newGraph()
@@ -45,10 +89,105 @@ func TestGraph_add(t *testing.T) {
 			graph.add(node.childId, node.parentId)
 		}
 		if !reflect.DeepEqual(graph.childs, item.childs) {
-			t.Errorf("Test #%d: expected graph children structure is incorrect: %#v", i, graph.childs)
+			t.Errorf("Test #%d: actual graph children structure is incorrect: %#v", i+1, graph.childs)
 		}
 		if !reflect.DeepEqual(graph.prnts, item.parents) {
-			t.Errorf("Test #%d: expected graph parents structure is incorrect: %#v", i, graph.prnts)
+			t.Errorf("Test #%d: actual graph parents structure is incorrect: %#v", i+1, graph.prnts)
+		}
+	}
+}
+
+func TestGraph_Renumber(t *testing.T) {
+	items := []struct{
+		nodes []node
+		childs map[int][]int
+		parents map[int][]int
+		currentNodeId, breakNodeId int
+	}{
+		// #1
+		{
+			[]node{
+				{1, 0},
+				{2, 1}, {6, 1},
+				{3, 2},
+				{4, 3},
+				{5, 4},
+			},
+			map[int][]int{
+				0: {1},
+				1: {2, 4},
+				2: {3},
+				3: {4},
+				4: {5},
+				5: {6},
+			},
+			map[int][]int{
+				1: {0},
+				2: {1},
+				3: {2},
+				4: {1, 3},
+				5: {4},
+				6: {5},
+			},
+			6, 4,
+		},
+		// #2
+		{
+			[]node{
+				{1, 0},
+				{2, 1}, {12, 1},
+				{3, 2}, {4, 2}, {5, 2},
+				{4, 5}, {6, 5}, {7, 5},
+				{7, 6}, {8, 6}, {9, 6}, {11, 6},
+				{2, 8}, {10, 8},
+				{10, 9},
+				{4, 11}, {5, 11},
+				{13, 12}, {14, 12},
+				{7, 13},
+			},
+			map[int][]int{
+				0: {1},
+				1: {2, 5},
+				2: {3, 4, 5},
+				5: {6, 7},
+				6: {10},
+				7: {8},
+				8: {4, 9, 10},
+				9: {10, 11, 12, 14},
+				11: {2, 13},
+				12: {13},
+				14: {4, 8},
+			},
+			map[int][]int{
+				1: {0},
+				2: {1, 11},
+				3: {2},
+				4: {2, 8, 14},
+				5: {1},
+				6: {5},
+				7: {5, 2, 14},
+				8: {7},
+				9: {8},
+				10: {8, 9, 6},
+				11: {9},
+				12: {9},
+				13: {11, 12},
+				14: {9},
+			},
+			14, 5,
+		},
+	}
+	for i, item := range items {
+		graph := newGraph()
+		for _, node := range item.nodes {
+			graph.add(node.childId, node.parentId)
+		}
+		graph.renumber(item.currentNodeId, item.breakNodeId)
+		if !reflect.DeepEqual(graph.childs, item.childs) {
+			t.Errorf("Test #%d: actual graph children structure is incorrect: %#v", i+1, graph.childs)
+		}
+		if !reflect.DeepEqual(graph.prnts, item.parents) {
+			t.Errorf("Test #%d: actual graph parents structure is incorrect: %#v", i+1, graph.prnts)
 		}
 	}
 }
